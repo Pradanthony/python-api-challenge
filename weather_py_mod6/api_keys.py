ee4aff2dc812d7ff5ba45e71416a6e3d2fe813de
Pradanthony <125171223+Pradanthony@users.noreply.github.com>
2023-03-27 18:16:40 -0400

    gitignore done

--- weather_py_mod6/api_keys.py
+++ weather_py_mod6/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = '7a377ca36b94f99bc3a054d1b400cdc0'
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "4a1c69abb08f4dd68d88dc4b8b70c092"
